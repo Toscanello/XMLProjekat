611cc9770568fc05034609299d3ee840baf980fd
svetaa99 <svetozar.vulin@gmail.com>
2022-01-08 21:25:05 +0100

    Adding initial project with parsing and writing/reading to the db.

---
+++ vakcinisoni/src/main/java/com/vakcinisoni/models/Book.java
@@ -0,0 +1,208 @@
+package com.vakcinisoni.models;
+
+import javax.xml.bind.annotation.*;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "title",
+    "author",
+    "year",
+    "price"
+})
+@XmlRootElement(name = "book")
+public class Book {
+
+    @XmlElement(required = true)
+    protected Title title;
+    @XmlElement(required = true)
+    protected List<String> author;
+    protected int year;
+    @XmlElement(required = true)
+    protected BigDecimal price;
+    @XmlAttribute(name = "category", required = true)
+    protected String category;
+
+    /**
+     * Gets the value of the title property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Title }
+     *     
+     */
+    public Title getTitle() {
+        return title;
+    }
+
+    /**
+     * Sets the value of the title property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Title }
+     *     
+     */
+    public void setTitle(Title value) {
+        this.title = value;
+    }
+
+    /**
+     * Gets the value of the author property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the author property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getAuthor().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link String }
+     * 
+     * 
+     */
+    public List<String> getAuthor() {
+        if (author == null) {
+            author = new ArrayList<String>();
+        }
+        return this.author;
+    }
+
+    /**
+     * Gets the value of the year property.
+     * 
+     */
+    public int getYear() {
+        return year;
+    }
+
+    /**
+     * Sets the value of the year property.
+     * 
+     */
+    public void setYear(int value) {
+        this.year = value;
+    }
+
+    /**
+     * Gets the value of the price property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    /**
+     * Sets the value of the price property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public void setPrice(BigDecimal value) {
+        this.price = value;
+    }
+
+    /**
+     * Gets the value of the category property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCategory() {
+        return category;
+    }
+
+    /**
+     * Sets the value of the category property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCategory(String value) {
+        this.category = value;
+    }
+
+
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlType(name = "", propOrder = {
+        "value"
+    })
+    public static class Title {
+
+        @XmlValue
+        protected String value;
+        @XmlAttribute(name = "lang")
+        protected String lang;
+
+        /**
+         * Gets the value of the value property.
+         * 
+         * @return
+         *     possible object is
+         *     {@link String }
+         *     
+         */
+        public String getValue() {
+            return value;
+        }
+
+        /**
+         * Sets the value of the value property.
+         * 
+         * @param value
+         *     allowed object is
+         *     {@link String }
+         *     
+         */
+        public void setValue(String value) {
+            this.value = value;
+        }
+
+        /**
+         * Gets the value of the lang property.
+         * 
+         * @return
+         *     possible object is
+         *     {@link String }
+         *     
+         */
+        public String getLang() {
+            return lang;
+        }
+
+        /**
+         * Sets the value of the lang property.
+         * 
+         * @param value
+         *     allowed object is
+         *     {@link String }
+         *     
+         */
+        public void setLang(String value) {
+            this.lang = value;
+        }
+
+    }
+
+}
