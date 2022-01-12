16676aaf260bf59c679497636cf6a03b84154934
svetaa99 <svetozar.vulin@gmail.com>
2022-01-12 20:33:18 +0100

    Implemented remaining Parsers.

--- vakcinisoni/src/main/java/com/vakcinisoni/models/Chain/ImmunizationAccordanceParser.java
+++ vakcinisoni/src/main/java/com/vakcinisoni/models/Chain/ImmunizationAccordanceParser.java
@@ -1,18 +1,17 @@
 package com.vakcinisoni.models.Chain;
 
-import com.vakcinisoni.models.DigitalCertificateRequest;
+import com.vakcinisoni.models.ImmunizationAccordance;
 import com.vakcinisoni.models.constants.Constants;
 import com.vakcinisoni.util.ObjectParser;
 import org.xmldb.api.modules.XMLResource;
 
-public class DigitalCertificateRequestParser extends ParserChain {
+public class ImmunizationAccordanceParser extends ParserChain {
 
     @Override
     public Object parse(XMLResource resource) {
-        setNext(new ImmunizationAccordanceParser());
-
+        setNext(new ImmunizationReportParser());
         try {
-            return (DigitalCertificateRequest) ObjectParser.parseToObject(resource, Constants.PATH_TO_MODELS);
+            return (ImmunizationAccordance) ObjectParser.parseToObject(resource, Constants.PATH_TO_MODELS);
         }
         catch (Exception e){
             return next.parse(resource);
